fe792a1e835f6c69416304f359a14a43afe6dd4e
Volko <vigilante.volko@gmail.com>
2016-06-03 07:39:58 -0400

    inital kek

---
+++ y0lych/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for y0lych project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "y0lych.settings")
+
+application = get_wsgi_application()
